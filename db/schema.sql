6c24e1b9783ab051d8d4bc0da7fd13c2e33cfb48
Kyle PC <kyle.dahl201@gmail.com>
2020-05-28 12:53:37 -0500

    create table

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,18 @@
-DROP DATABASE IF EXISTS employees;\
+DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 
 
 
 USE employees;
+
+
+
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+
+
+
 
